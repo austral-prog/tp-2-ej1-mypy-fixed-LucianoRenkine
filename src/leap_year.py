6ac20e2be618e6b9e1ab465bc59ba76896dd0071
renki <renkineluciano1@gmail.com>
2024-08-24 23:05:55 -0300

    Ejercicio 1 del TP 2.

--- src/leap_year.py
+++ src/leap_year.py
@@ -1,8 +1,11 @@
 def is_leap_year():
     year: int = int(input("> Ingrese un año: "))
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        print(f"{year} es bisiesto")
+        print(f"El año {year} es bisiesto")
         return True
     else:
-        print(f"{year} no es bisiesto")
+        print(f"El año {year} no es bisiesto")
         return False
+
+is_leap_year()
+
--- src/maximums.py
+++ src/maximums.py
@@ -14,7 +14,7 @@ def max_of_two(x: int, y: int):
 def max_of_three(x: int, y: int, z: int):
     """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
     int_list: List[int] = [x, y, z]
-    biggest: int = 0
+    biggest: int = x
     for i in int_list:
         if i > biggest:
             biggest: int = i
